0b98a7fb7424439271483d4fa4b6b0993a4fb033
wesleyweng <wesleyweng@nineder.com>
2023-10-03 20:31:25 +0800

    feat: add redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux';
 import App from './App.tsx'
+import store from './store';
 import './index.scss'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-underscore-dangle */
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+let preloadedState = null;
+if (process.env.NODE_ENV === 'development') {
+  // @ts-ignore
+  preloadedState = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+}
+const store = createStore(reducers, preloadedState);
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
