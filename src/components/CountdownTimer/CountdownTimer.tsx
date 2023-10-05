a42baecd99d0e77a0cfa7b54040bfdfda133e2e8
wesleyweng <wesleyweng@nineder.com>
2023-10-05 14:28:14 +0800

    style: modify Countdown Timer style

--- src/components/CountdownTimer/CountdownTimer.tsx
+++ src/components/CountdownTimer/CountdownTimer.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./CountdownTimer.module.scss";
 
 interface NavTabItem {
     id: string;
@@ -15,14 +16,18 @@ type TimeProps = {
 };
 
 const CountdownTimer: React.FC<TimeProps> = (props)  => {
-    const { value, isActive, navTab, duration, onUpdateTimeMode, onChangeActiveStatus } = props;
+    const { value, isActive, navTab, onUpdateTimeMode, onChangeActiveStatus } = props;
+
+    const [activeTab, setActiveTab] = useState<string>("Pomodoro")
 
     const atChangeActive = ():void => {
         onChangeActiveStatus();
     }
 
     const atChangeCountDownMode = (value: string): void => {
+        console.log(value);
         onUpdateTimeMode(value)
+        setActiveTab(value)
     }
  
     return (
@@ -30,29 +35,29 @@ const CountdownTimer: React.FC<TimeProps> = (props)  => {
         <section className="flex w-full h-full">
             <article className="w-[600px] h-[350px] bg-black m-auto rounded-[8px]">
                 <ul className="w-auto flex space-x-3 text-center text-xl cursor-pointer rounded-default justify-center items-center gap-6 menu">
-                    {navTab.map((tab) => {
-                        return <li onClick={() => atChangeCountDownMode(tab.title)} className="block item h-auto hover:text-cyan-500 border-transparent border-b-2 hover:border-b-cyan-500 hover:transition duration-500" key={tab.id}>{tab.title}</li>
-                    })}
-                    {/* <li 
-                        @click="onChangeTab(item.title)" 
-                        :className="{ 'tab-active': activeTab === item.title }"
-                        class="block item h-auto hover:text-cyan-500 border-transparent border-b-2 hover:border-b-cyan-500 hover:transition duration-500"
-                        v-for="item in COUNTDOWN_MODE"
-                        :key="item.id"
-                    >
-                        {{ item.title }}
-                    </li> */}
+                    {navTab.map((item) => (
+                        <li 
+                            onClick={() => atChangeCountDownMode(item.title)}
+                            className={`block item h-auto border-transparent border-b-2 ${activeTab === item.title ? styles['tab-active'] : ''} hover:text-cyan-500 hover:border-b-cyan-500 hover:transition duration-500`}
+                            key={item.id}
+                        >
+                            {item.title}
+                        </li>
+                    ))}
                 </ul>
-                <div className="box relative overflow-hidden h-[350px] bg-custom-gray rounded-[8px] mt-10 flex justify-center items-center flex-wrap ">
-                <div className="form absolute top-[4px] right-[4px] bottom-[4px] left-[4px] z-[2] flex-col flex overflow-hidden">
-                    <p className="text-9xl w-full text-center mt-[40px]">{value}</p>
-                    <button className="btn w-[100px] relative inline-block text-light text-base font-medium border-none py-[10xp] px-[25px] m-auto leading-10 shadow-activeButton transition-all duration-300 ease-in hover:bg-transparent">
-                        <span className="relative block w-full h-full" onClick={atChangeActive}>
-                            {`${!isActive ? "Start" : "Pause"}`}
-                        </span>
-                    </button>
+
+                
+
+                <div className={`${styles['box']} box relative overflow-hidden h-[350px] bg-custom-gray rounded-[8px] mt-10 flex justify-center items-center flex-wrap`}>
+                    <div className="form absolute top-[4px] right-[4px] bottom-[4px] left-[4px] z-[2] bg-gray-800 flex-col flex overflow-hidden">
+                        <p className="text-9xl w-full text-center mt-[40px]">{value}</p>
+                        <button onClick={atChangeActive} className={`${styles['btn']} btn w-[100px] relative inline-block text-light text-base font-medium border-none py-[10xp] px-[25px] m-auto leading-10 shadow-activeButton transition-all duration-300 ease-in hover:bg-transparent`}>
+                            <span className="relative block w-full h-full" >
+                                {`${!isActive ? "Start" : "Pause"}`}
+                            </span>
+                        </button>
+                    </div>
                 </div>
-            </div>
                 
             </article>
         </section>
