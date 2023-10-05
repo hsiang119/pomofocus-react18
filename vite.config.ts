d7bc7ddc25e1873fdaca45994214d1679ab7ff09
wesleyweng <wesleyweng@nineder.com>
2023-10-05 14:27:02 +0800

    feat: modify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+// import { fileURLToPath, URL } from 'node:url'
+import path from "path";
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      // components: `${path.resolve(__dirname, "./src/components/")}`,
+      // contaniers: `${path.resolve(__dirname, "./src/contaniers/")}`,
+      // public: `${path.resolve(__dirname, "./public/")}`,
+      // pages: path.resolve(__dirname, "./src/pages"),
+      // types: `${path.resolve(__dirname, "./src/@types")}`,
+    },
+  }
 })
