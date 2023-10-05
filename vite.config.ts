99a5922b34b2a6a05f7e38c887e4f4f354693425
wesleyweng <wesleyweng@nineder.com>
2023-10-05 14:32:35 +0800

    feat: add gh-pages package

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pomofocus-react18/',
   plugins: [react()],
   resolve: {
     alias: {
