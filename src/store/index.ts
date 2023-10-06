e07acb694952bee3ff6536f32ca61dc997a68344
wesleyweng <wesleyweng@nineder.com>
2023-10-06 10:30:23 +0800

    fix: bug fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,11 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
-// let preloadedState = null;
-// if (process.env.NODE_ENV === 'development') {
-//   // @ts-ignore
-//   preloadedState = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-// }
+let preloadedState = null;
+if (process.env.NODE_ENV === 'development') {
+  // @ts-ignore
+  preloadedState = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+}
 // const devToolsExtension = process.env.NODE_ENV === 'development' && window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : undefined;
 const store = createStore(reducers);
 
