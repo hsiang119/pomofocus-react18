ffc67ee723b75cc9ec91f26bf2f517939ab02afe
wesleyweng <wesleyweng@nineder.com>
2023-10-06 14:01:55 +0800

    fix: fix path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/pomofocus-react18/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/pomofocus-react18/' : './',
   plugins: [react()],
   resolve: {
     alias: {
