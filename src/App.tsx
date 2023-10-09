84d991cb89d4716af244d757aa5f40070b83aebf
wesleyweng <wesleyweng@nineder.com>
2023-10-09 19:13:21 +0800

    fix: fix pages not found

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Home from "./components/Home";
 export default function App() {
 
   return (
-    <Router>
+    <Router basename={'/pomofocus-react18/'}>
       <div className="App xl:max-w-1200 lg:max-w-1000 md:max-w-600 w-full mx-auto my-0 h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
